375fc6807bba96b03687b0c63771f97f235222c3
cheluvesha <cheluvesh.b@gmail.com>
2020-08-02 13:02:50 +0530

    sloving switch case is added.

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -73,3 +73,34 @@ public static void main(String args[]){
 }
 }
 
+//UC-4
+
+class SwitchCaseWage{
+        final static int isPartTime=0;
+        final static int isFullTime=1;
+        int salary=0;
+        int empRatePerHour=20;
+        int empHours;
+public void calculate(){
+        int check=(int)(Math.floor(Math.random()*10)%2);
+        switch(check){
+                case isPartTime :
+                        empHours=4;
+                        break;
+                case isFullTime :
+                        empHours=8;
+                        break;
+                default :
+                        empHours=0;
+                }
+       salary=empRatePerHour*empHours;
+       System.out.println(salary);
+}
+}
+
+public class WageUsingSwitch{
+public static void main(String aargs[]){
+        SwitchCaseWage wage=new SwitchCaseWage();
+        wage.calculate();
+}
+}
