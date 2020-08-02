f4a032ec418f6a2ed33830678836f7c67ce16ad3
cheluvesha <cheluvesh.b@gmail.com>
2020-08-02 12:59:53 +0530

    Part and full time wage calculation is added

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -46,3 +46,30 @@ public static void main(String args[]){
 }
 }
 
+//UC-3
+
+class Wage{
+        final int partTime=0;
+        int salary=0;
+        final int empRatePerHour=20;
+        int empHours;
+public void calculateWage(){
+        int check=(int)(Math.floor(Math.random()*10)%2);
+        if(partTime==check){
+             empHours=4;
+         }
+         else{
+             empHours=8;
+         }
+         salary=empRatePerHour*empHours;
+         System.out.println(salary);
+}
+}
+
+public class PartAndFullTimeWage{
+public static void main(String args[]){
+        Wage wage=new Wage();
+        wage.calculateWage();
+}
+}
+
